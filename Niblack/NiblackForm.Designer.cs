1e65469e1921163e9baa706d6709aab1bb966261
Bence Desvari <bence.desvari@gmail.com>
2020-12-06 23:28:51 +0100

    Sobel operátor / kinézet
    Sobel operátor implementálva
    Létre lett hozva még 2 ablak, 1 a Niblack eredményének, 1 a Sobel operátor eredményének
    Sobel operátor -> AfterSobel.png

--- Niblack/NiblackForm.Designer.cs
+++ Niblack/NiblackForm.Designer.cs
@@ -31,44 +31,52 @@
             this.codeBut = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PB1)).BeginInit();
             this.fLP1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(248, 251);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(189, 343);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.Size = new System.Drawing.Size(62, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Progress:";
+            this.label4.Text = "Eredeti";
             // 
             // PoB1
             // 
-            this.PoB1.Location = new System.Drawing.Point(307, 328);
-            this.PoB1.Margin = new System.Windows.Forms.Padding(2);
+            this.PoB1.Location = new System.Drawing.Point(14, 645);
             this.PoB1.Name = "PoB1";
-            this.PoB1.Size = new System.Drawing.Size(143, 20);
+            this.PoB1.Size = new System.Drawing.Size(440, 31);
             this.PoB1.TabIndex = 7;
             this.PoB1.Click += new System.EventHandler(this.PoB1_Click);
             // 
             // PB1
             // 
-            this.PB1.Location = new System.Drawing.Point(2, 2);
-            this.PB1.Margin = new System.Windows.Forms.Padding(2);
+            this.PB1.Location = new System.Drawing.Point(3, 3);
             this.PB1.Name = "PB1";
-            this.PB1.Size = new System.Drawing.Size(452, 236);
+            this.PB1.Size = new System.Drawing.Size(437, 263);
             this.PB1.TabIndex = 0;
             this.PB1.TabStop = false;
+            this.PB1.Click += new System.EventHandler(this.PB1_Click);
             // 
             // OpenBut
             // 
-            this.OpenBut.Location = new System.Drawing.Point(10, 287);
-            this.OpenBut.Margin = new System.Windows.Forms.Padding(2);
+            this.OpenBut.Location = new System.Drawing.Point(12, 434);
             this.OpenBut.Name = "OpenBut";
-            this.OpenBut.Size = new System.Drawing.Size(105, 31);
+            this.OpenBut.Size = new System.Drawing.Size(158, 48);
             this.OpenBut.TabIndex = 1;
             this.OpenBut.Text = "Megnyitás";
             this.OpenBut.UseVisualStyleBackColor = true;
@@ -77,54 +85,48 @@
             // fLP1
             // 
             this.fLP1.Controls.Add(this.PB1);
-            this.fLP1.Location = new System.Drawing.Point(9, 10);
-            this.fLP1.Margin = new System.Windows.Forms.Padding(2);
+            this.fLP1.Location = new System.Drawing.Point(14, 15);
             this.fLP1.Name = "fLP1";
-            this.fLP1.Size = new System.Drawing.Size(454, 264);
+            this.fLP1.Size = new System.Drawing.Size(440, 313);
             this.fLP1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(159, 300);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(202, 462);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.Size = new System.Drawing.Size(22, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "k:";
             // 
             // kBox
             // 
-            this.kBox.Location = new System.Drawing.Point(178, 300);
-            this.kBox.Margin = new System.Windows.Forms.Padding(2);
+            this.kBox.Location = new System.Drawing.Point(231, 462);
             this.kBox.Name = "kBox";
-            this.kBox.Size = new System.Drawing.Size(76, 20);
+            this.kBox.Size = new System.Drawing.Size(112, 26);
             this.kBox.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 328);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(177, 502);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(49, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Area:";
             // 
             // areaBox
             // 
-            this.areaBox.Location = new System.Drawing.Point(178, 328);
-            this.areaBox.Margin = new System.Windows.Forms.Padding(2);
+            this.areaBox.Location = new System.Drawing.Point(231, 502);
             this.areaBox.Name = "areaBox";
-            this.areaBox.Size = new System.Drawing.Size(76, 20);
+            this.areaBox.Size = new System.Drawing.Size(112, 26);
             this.areaBox.TabIndex = 4;
             // 
             // codeBut
             // 
-            this.codeBut.Location = new System.Drawing.Point(9, 323);
-            this.codeBut.Margin = new System.Windows.Forms.Padding(2);
+            this.codeBut.Location = new System.Drawing.Point(11, 493);
             this.codeBut.Name = "codeBut";
-            this.codeBut.Size = new System.Drawing.Size(106, 29);
+            this.codeBut.Size = new System.Drawing.Size(159, 45);
             this.codeBut.TabIndex = 5;
             this.codeBut.Text = "Niblack ";
             this.codeBut.UseVisualStyleBackColor = true;
@@ -133,29 +135,95 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(186, 284);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(238, 434);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.Size = new System.Drawing.Size(96, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Beállítások:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(348, 300);
+            this.label5.Location = new System.Drawing.Point(177, 679);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.Size = new System.Drawing.Size(82, 20);
             this.label5.TabIndex = 8;
             this.label5.Text = "Progress:";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(535, 15);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(440, 313);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.flowLayoutPanel1.SetFlowBreak(this.pictureBox1, true);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(437, 272);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Sobel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(727, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Niblack";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.pictureBox3);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(535, 363);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(440, 313);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(437, 272);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(727, 693);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Sobel";
+            // 
             // Niblack_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.ClientSize = new System.Drawing.Size(472, 366);
+            this.ClientSize = new System.Drawing.Size(1032, 722);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.codeBut);
@@ -167,12 +235,15 @@
             this.Controls.Add(this.OpenBut);
             this.Controls.Add(this.PoB1);
             this.Controls.Add(this.label4);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Niblack_Form";
             this.Text = "Kép feldolgozás (Niblack eljárás)";
             this.Load += new System.EventHandler(this.Niblack_Form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.PB1)).EndInit();
             this.fLP1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,7 +263,13 @@
         private System.Windows.Forms.Label label4;
         public System.Windows.Forms.ProgressBar PoB1;
         private System.Windows.Forms.Label label5;
-        
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Niblack/NiblackForm.cs
+++ Niblack/NiblackForm.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.ComponentModel;
 using System.Threading;
 using System.Diagnostics;
-using System.Threading;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
 
 namespace Niblack
 {
@@ -23,6 +30,8 @@ namespace Niblack
         public int area  = 2;
         private Bitmap VerifiedImage;
         
+        
+
         private void OpenBut_Click(object sender, EventArgs e)  
         {
             
@@ -64,8 +73,10 @@ namespace Niblack
                 Int32.TryParse(areaBox.Text, out area);
                 PoB1.Value = NiblackAlgorithm.GetBar();
                 Bitmap afterNibleck = NiblackAlgorithm.Binarization(VerifiedImage, k, area);
-                PB1.Image = afterNibleck;
-                PB1.Show();
+                pictureBox1.Size = afterNibleck.Size;
+                pictureBox1.Image = afterNibleck;
+                pictureBox1.Show();
+                flowLayoutPanel1.AutoScroll = true;
                 afterNibleck.Save("AfterNiblack.png");
                 Console.Out.WriteLine(NiblackAlgorithm.GetBar());
             PoB1.Value = NiblackAlgorithm.GetBar();
@@ -93,6 +104,175 @@ namespace Niblack
         private void Niblack_Form_Load(object sender, EventArgs e)
         {
             
+        }
+
+        private void PB1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        ////////////////////////   2020.12.06 - Sobel Operator implementálás
+
+        //Sobel x-tengely pixel csere kernel
+        private static double[,] xSobel
+        {
+            get
+            {
+                return new double[,]
+                {
+                    { -1, 0, 1 },
+                    { -2, 0, 2 },
+                    { -1, 0, 1 }
+                };
+            }
+        }
+
+        //Sobel y-tengely pixel csere kernel
+        private static double[,] ySobel
+        {
+            get
+            {
+                return new double[,]
+                {
+                    {  1,  2,  1 },
+                    {  0,  0,  0 },
+                    { -1, -2, -1 }
+                };
+            }
+        }
+        static public Bitmap ConvolutionFilter(Bitmap afterNibleck, double[,] xkernel, double[,] ykernel, double factor = 1, int bias = 0, bool grayscale = false)
+        {
+
+            //Kép méretek tárolása
+            int width = afterNibleck.Width;
+            int height = afterNibleck.Height;
+
+            //Lock
+            BitmapData srcData = afterNibleck.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            //Bite számítás - 32 bytes / pixel x kép szélesség x kép magasság ->  32bpp 
+            int bytes = srcData.Stride * srcData.Height;
+
+            //Mátrix létrehozása
+            byte[] pixelBuffer = new byte[bytes];
+            byte[] resultBuffer = new byte[bytes];
+
+            //Első pixel cím szerzés
+            IntPtr srcScan0 = srcData.Scan0;
+
+            //Adattárolás 1 bitnyi adatba
+            Marshal.Copy(srcScan0, pixelBuffer, 0, bytes);
+
+            //Unlock
+            afterNibleck.UnlockBits(srcData);
+
+            //Szürkeárnyalat (ha szükséges)
+            if (grayscale == true)
+            {
+                float rgb = 0;
+                for (int i = 0; i < pixelBuffer.Length; i += 4)
+                {
+                    rgb = pixelBuffer[i] * .21f;
+                    rgb += pixelBuffer[i + 1] * .71f;
+                    rgb += pixelBuffer[i + 2] * .071f;
+                    pixelBuffer[i] = (byte)rgb;
+                    pixelBuffer[i + 1] = pixelBuffer[i];
+                    pixelBuffer[i + 2] = pixelBuffer[i];
+                    pixelBuffer[i + 3] = 255;
+                }
+            }
+
+            //Változó létrehozása pixel adatoknak (kernel)
+            double xr = 0.0;
+            double xg = 0.0;
+            double xb = 0.0;
+            double yr = 0.0;
+            double yg = 0.0;
+            double yb = 0.0;
+            double rt = 0.0;
+            double gt = 0.0;
+            double bt = 0.0;
+
+
+            int filterOffset = 1;
+            int calcOffset = 0;
+            int byteOffset = 0;
+
+            //Start with the pixel that is offset 1 from top and 1 from the left side
+            //this is so entire kernel is on your image
+            for (int OffsetY = filterOffset; OffsetY < height - filterOffset; OffsetY++)
+            {
+                for (int OffsetX = filterOffset; OffsetX < width - filterOffset; OffsetX++)
+                {
+                    //reset rgb values to 0
+                    xr = xg = xb = yr = yg = yb = 0;
+                    rt = gt = bt = 0.0;
+
+                    //position of the kernel center pixel
+                    byteOffset = OffsetY * srcData.Stride + OffsetX * 4;
+
+                    //kernel calculations
+                    for (int filterY = -filterOffset; filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset; filterX <= filterOffset; filterX++)
+                        {
+                            calcOffset = byteOffset + filterX * 4 + filterY * srcData.Stride;
+                            xb += (double)(pixelBuffer[calcOffset]) * xkernel[filterY + filterOffset, filterX + filterOffset];
+                            xg += (double)(pixelBuffer[calcOffset + 1]) * xkernel[filterY + filterOffset, filterX + filterOffset];
+                            xr += (double)(pixelBuffer[calcOffset + 2]) * xkernel[filterY + filterOffset, filterX + filterOffset];
+                            yb += (double)(pixelBuffer[calcOffset]) * ykernel[filterY + filterOffset, filterX + filterOffset];
+                            yg += (double)(pixelBuffer[calcOffset + 1]) * ykernel[filterY + filterOffset, filterX + filterOffset];
+                            yr += (double)(pixelBuffer[calcOffset + 2]) * ykernel[filterY + filterOffset, filterX + filterOffset];
+                        }
+                    }
+
+                    //total rgb values for this pixel
+                    bt = Math.Sqrt((xb * xb) + (yb * yb));
+                    gt = Math.Sqrt((xg * xg) + (yg * yg));
+                    rt = Math.Sqrt((xr * xr) + (yr * yr));
+
+                    //set limits, bytes can hold values from 0 up to 255;
+                    if (bt > 255) bt = 255;
+                    else if (bt < 0) bt = 0;
+                    if (gt > 255) gt = 255;
+                    else if (gt < 0) gt = 0;
+                    if (rt > 255) rt = 255;
+                    else if (rt < 0) rt = 0;
+
+                    //set new data in the other byte array for your image data
+                    resultBuffer[byteOffset] = (byte)(bt);
+                    resultBuffer[byteOffset + 1] = (byte)(gt);
+                    resultBuffer[byteOffset + 2] = (byte)(rt);
+                    resultBuffer[byteOffset + 3] = 255;
+                }
+            }
+
+            //Create new bitmap which will hold the processed data
+            Bitmap resultImage = new Bitmap(width, height);
+
+            //Lock bits into system memory
+            BitmapData resultData = resultImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            //Copy from byte array that holds processed data to bitmap
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+
+            //Unlock bits from system memory
+            resultImage.UnlockBits(resultData);
+
+            //Return processed image
+            return resultImage;
+        }
+
+    
+    private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap afterSobel = ConvolutionFilter(VerifiedImage, xSobel, ySobel, 1.0, 0, true);
+            Invalidate();
+            pictureBox3.Size = afterSobel.Size;
+            pictureBox3.Image = afterSobel;
+            pictureBox3.Show();
+            flowLayoutPanel2.AutoScroll = true;
+            afterSobel.Save("AfterSobel.png");
         }
     }
 }
